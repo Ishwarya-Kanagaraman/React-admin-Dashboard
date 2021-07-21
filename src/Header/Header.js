94cd06fb781bb77209fb9fa701af038092efdc21
Ishwarya-Kanagaraman <ishwaryaraman324@gmail.com>
2021-07-21 19:19:55 +0530

    cleared error

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,4 @@
-import React ,{useState} from "react";
+import React from "react";
 import { Badge } from "react-bootstrap";
 import AnimationsPage from "../pages/AnimationsPage";
 import "./Header.css";
